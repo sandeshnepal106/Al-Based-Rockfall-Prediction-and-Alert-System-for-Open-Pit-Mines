645a8c6fa6a319cc2a1713c8f5c7f9738b2b3a48
Adwityakumar <adwityakumar226@gmail.com>
2025-09-06 06:34:34 +0530

    create react app

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="container">
+      <div className="logos">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount(count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
